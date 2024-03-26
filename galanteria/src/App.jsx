1528dc97985d4a8e470ff9ccb634534a04fcf84c
blendshalaa <90574525+blendshalaa@users.noreply.github.com>
2024-03-26 01:13:48 +0100

    variabel and file restructure

--- galanteria/src/App.jsx
+++ galanteria/src/App.jsx
@@ -1,10 +1,10 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import HomePage from "./Components/HomePage/HomePage";
+import HomePage from "./Pages/HomePage/HomePage";
 import NavBar from "./Components/NavBar/NavBar";
-import Footer from "./Components/Footer/Footer";
-import Aboutus from "./Components/Aboutus/Aboutus";
-import Ideas from "./Components/Ideas/Ideas";
-import Contact from "./Components/Contact/Contact";
+import Footer from "./Pages/Footer/Footer";
+import Aboutus from "./Pages/Aboutus/Aboutus";
+import Ideas from "./Pages/Ideas/Ideas";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
 
