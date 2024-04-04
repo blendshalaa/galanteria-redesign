cc9d53f6d4342c6fe2e49db557a35911b5cc6a8e
blendshalaa <90574525+blendshalaa@users.noreply.github.com>
2024-04-04 04:17:11 +0200

    startedFooter

--- galanteria/src/Pages/Contact/Contact.jsx
+++ galanteria/src/Pages/Contact/Contact.jsx
@@ -1,10 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './Contact.scss'
+import NavBar from '../../Components/NavBar/NavBar'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <NavBar/>
+      <div className='contact-header'>
+        <div>
+          <h1>Contact Us</h1>
+          <p>Get in touch and let us know how we can help</p>
+        </div>
+        <div>
+          
+
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
