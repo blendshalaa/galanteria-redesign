20535423ef278141a9f718d64793c88d6750af07
Blerta Jusufii <blertamicrosoft10@gmail.com>
2024-04-06 01:46:06 +0200

    products pages

--- galanteria/src/data/products.js
+++ galanteria/src/data/products.js
@@ -9,9 +9,9 @@ const firstphotos = [
 const dataProducts = {
     "sq": {
         "first": {
-            category: "Beds",
+            category: "Shtretër",
             name: "Grenen Light-beige velor Tori \n 200x140",
-            description: "Formed by centuries-old Danish traditions, the \n modern Scandinavian style is reflected in the \n Grenen bed, named after the cape at the \n northernmost point of the country. As the \n Baltic Sea meets the North here, so comfort \n and convenience are found with the quality \n and reliability of the Grenen bed.",
+            description: "I formuar nga traditat shekullore daneze, stili modern skandinav pasqyrohet në shtratin Grenen, i quajtur sipas pelerinës në pikën më veriore të vendit. Ndërsa Deti Baltik takohet me Veriun këtu, kështu komoditeti dhe komoditeti gjenden me cilësinë dhe besueshmërinë e shtratit Grenen.",
             photos: firstphotos
         },
     },
@@ -25,9 +25,9 @@ const dataProducts = {
     },
     "de": {
         "first": {
-            category: "Beds",
+            category: "Betten",
             name: "Grenen Light-beige velor Tori \n 200x140",
-            description: "Formed by centuries-old Danish traditions, the \n modern Scandinavian style is reflected in the \n Grenen bed, named after the cape at the \n northernmost point of the country. As the \n Baltic Sea meets the North here, so comfort \n and convenience are found with the quality \n and reliability of the Grenen bed.",
+            description: "Geprägt durch jahrhundertealte dänische Traditionen spiegelt sich der moderne skandinavische Stil im Grenen-Bett wider, benannt nach dem Kap am nördlichsten Punkt des Landes. Da hier die Ostsee auf den Norden trifft, vereinen sich Komfort und Bequemlichkeit mit der Qualität und Zuverlässigkeit des Grenen-Bettes.",
             photos: firstphotos
         },
     }
