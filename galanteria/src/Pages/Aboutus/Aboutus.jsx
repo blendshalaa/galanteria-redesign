c3ca20d20359ace1d2b04419654cdc7abe4a0561
Blerta Jusufii <blertamicrosoft10@gmail.com>
2024-05-26 13:07:49 +0200

    aboutus and contact pages done

--- galanteria/src/Pages/Aboutus/Aboutus.jsx
+++ galanteria/src/Pages/Aboutus/Aboutus.jsx
@@ -1,67 +1,74 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useContext } from 'react'
 import './Aboutus.scss'
 import NavBar from '../../Components/NavBar/NavBar'
-import aboutimg from '../../assets/images/aboutus.png'
-import periudhaimg from '../../assets/images/p3.png'
-import periudhaimg1 from '../../assets/images/periudha1.png'
+import aboutimg from '../../assets/images/o001.jpg'
+// import periudhaimg from '../../assets/images/p3.png'
+// import periudhaimg1 from '../../assets/images/periudha1.png'
 import Footer from '../Footer/Footer'
+import language from '../../lang'
+import { Context } from '../../Components/Context/Products'
+
+
 
 
 
 const Aboutus = () => {
+
+  const [{ lang }] = useContext(Context);
+
   return (
     <div className='mainAbout'>
-      <NavBar/>
+      <NavBar />
       <div className='about-us-header'>
         <div className='aboutusheaderP'>
-        <h1 className='aboutus-h1'>Who we are</h1>
-        <p className='aboutus-p'>At Galanteria, we’re here to help you create a space that reflects you and what<br></br> you love. We’ll put it all together for you. Free one-on-one design help, in our<br></br> store or in your home!</p>
+          <h1 className='aboutus-h1'>{language[lang]?.about[0].right1}</h1>
+          <p className='aboutus-p'>{language[lang]?.about[0].right2}</p>
         </div>
         <div className='img-container'>
-        <img className='aboutimg' src={aboutimg}></img>
+          <img className='aboutimg' src={aboutimg}></img>
         </div>
         <div className='statsTable'>
-  <div className='stats-text'>
-    <div>
-      <h2 className='stats-h2'>600+</h2>
-      <p className='stats-p'>Models Finished</p>
-    </div>
-    <div>
-      <h2 className='stats-h2'>200+</h2>
-      <p className='stats-p'>Unique assortments</p>
-    </div>
-    <div>
-      <h2 className='stats-h2'>30+</h2>
-      <p className='stats-p'>Across the country</p>
-    </div>
-  </div>
-</div>
+          <div className='stats-text'>
+            <div>
+              <h2 className='stats-h2'>1000+</h2>
+              <p className='stats-p'>{language[lang]?.about[0].left1}</p>
+            </div>
+            <div>
+              <h2 className='stats-h2'>200+</h2>
+              <p className='stats-p'>{language[lang]?.about[0].left2}</p>
+            </div>
+            <div>
+              <h2 className='stats-h2'>15+</h2>
+              <p className='stats-p'>{language[lang]?.about[0].left3}</p>
+            </div>
+          </div>
+        </div>
 
-</div>
-<div className='solutions'>
-  <h2 className='solution-h2'>WE ARE BEST SOLUTIONS<br/> FOR YOUR HOME</h2>
+      </div>
+      <div className='solutions'>
+        {/* <h2 className='solution-h2'>WE ARE BEST SOLUTIONS<br/> FOR YOUR HOME</h2> */}
 
-  <div className='boxes'>
-    <div className='box'>
-      <h2 className='box-h2'>OUR VISION</h2>
-      <p className='box-p'>Excellence and development of <br/>human, technical and technological<br/> capacities, for the fulfillment of the <br/>companys mission.</p>
-    </div>
-    <div className='box'>
+        <div className='boxes'>
+          <div className='box'>
+            <h2 className='box-h2'>{language[lang]?.about[0].bottom1}</h2>
+            <p className='box-p'>{language[lang]?.about[0].bottom12}</p>
+          </div>
+          {/* <div className='box'>
       <h2 className='box-h2'>WHAT WE OFFER</h2>
       <p className='box-p'>Office furniture, School Furniture,<br/>  University Furniture, Hotel Furniture<br/>  Working desks, shelves, cupboards,<br/> conference  tables, hangnails etc.</p>
-    </div>
-    <div className='box'>
-      <h2 className='box-h2'>WHAT WE OFFER</h2>
-      <p className='box-p'>Office furniture, School Furniture,<br/>  University Furniture, Hotel Furniture<br/>  Working desks, shelves, cupboards,<br/> conference  tables, hangnails etc.</p>
-    </div>
-  </div>
-</div>
-<div className='line'>
+    </div> */}
+          <div className='box'>
+            <h2 className='box-h2'>{language[lang]?.about[0].bottom2}</h2>
+            <p className='box-p'>{language[lang]?.about[0].bottom21}</p>
+          </div>
+        </div>
+      </div>
+      <div className='line'>
 
-</div>
+      </div>
 
-<div className='periudhat'>
+      {/* <div className='periudhat'>
   <div className='periudha-1'>
     <div className='pimg-container'>
       <img src={periudhaimg} alt='foto'></img>
@@ -82,12 +89,12 @@ const Aboutus = () => {
     </div>
   </div>
 
-</div>
+</div> */}
 
 
 
 
-<div className='periudhat-2'>
+      {/* <div className='periudhat-2'>
 <div className='periudha-1'>
     <div className='pimg-container'>
       <img src={periudhaimg} alt='foto'></img>
@@ -107,13 +114,13 @@ const Aboutus = () => {
       <img src={periudhaimg1}></img>
     </div>
   </div>
-</div>
-<div className='line'>
-</div>
+</div> */}
+      {/* <div className='line'>
+</div> */}
 
-<Footer/>
+      <Footer />
 
-      </div>
+    </div>
   )
 }
 
